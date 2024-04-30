99500299b7d08aaa53c178f8bbeaa6e7401ae234
Edward-Odey <edwardodey04@gmail.com>
2024-04-30 14:36:18 +0100

    5-http.js: added try block for failed file reading

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,47 +1,5 @@
 const http = require('http');
-const fs = require('fs');
-
-function countStudents(path) {
-  let lines = '';
-
-  try {
-    const data = fs.readFileSync(path, 'utf8');
-
-    lines = (data.split('\n')).slice(1);
-  } catch (err) {
-    throw new Error('Cannot load the database');
-  }
-
-  const values = {};
-
-  values.len = 0;
-
-  lines.forEach((line) => {
-    if (line !== '') {
-      values.len += 1;
-
-      const subValues = line.split(',');
-      const field = subValues.pop();
-
-      if (field in values) {
-        values[field].total += 1;
-        values[field].students.push(subValues[0]);
-      } else {
-        values[field] = {};
-        values[field].total = 1;
-        values[field].students = [subValues[0]];
-      }
-    }
-  });
-
-  console.log(`Number of students: ${values.len}`);
-  for (const key in values) {
-    if (values[key].students) {
-      const list = values[key].students.join(', ');
-      console.log(`Number of students in ${key}: ${values[key].total}. List: ${list}`);
-    }
-  }
-}
+const countStudents = require('./2-read_file');
 
 const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
@@ -58,13 +16,16 @@ const app = http.createServer((req, res) => {
       buffer.push(message);
     };
 
-    countStudents(process.argv[2]);
-
-    console.log = mainLog;
-
     res.write('This is the list of our students\n');
+    try {
+      countStudents(process.argv[2]);
 
-    res.write(buffer.join('\n'));
+      console.log = mainLog;
+
+      res.write(buffer.join('\n'));
+    } catch (err) {
+      // Do nothing Here
+    }
   }
 
   res.end();
