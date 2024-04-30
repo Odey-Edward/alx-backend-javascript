f7be17bad184b86f42ed7bab3eda90a9cac02426
Edward-Odey <edwardodey04@gmail.com>
2024-04-30 14:07:20 +0100

    5-http.js: created more complex http server for app

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const countStudents = require('./2-read_file');
+
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  const { url } = req;
+
+  if (url === '/') {
+    res.write('Hello Holberton School!');
+  } else if (url === '/students') {
+    const buffer = [];
+    const mainLog = console.log;
+
+    console.log = (message) => {
+      buffer.push(message);
+    };
+
+    countStudents(process.argv[2]);
+
+    console.log = mainLog;
+
+    res.write(buffer.join('\n'));
+  }
+
+  res.end();
+});
+
+app.listen(1245);
+
+module.exports = app;
