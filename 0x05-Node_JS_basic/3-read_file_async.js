2ac82723759bab76c90e21221d737d0716151b31
Edward-Odey <edwardodey04@gmail.com>
2024-04-30 12:51:45 +0100

    3-read_file_async.js: created countStudents fro asynchronous file reading

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+
+async function countStudents(path) {
+  let lines = '';
+
+  try {
+    const data = await fs.promises.readFile(path, 'utf8');
+
+    lines = (data.split('\n')).slice(1);
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+
+  const values = {};
+
+  values.len = 0;
+
+  lines.forEach((line) => {
+    if (line !== '') {
+      values.len += 1;
+
+      const subValues = line.split(',');
+      const field = subValues.pop();
+
+      if (field in values) {
+        values[field].total += 1;
+        values[field].students.push(subValues[0]);
+      } else {
+        values[field] = {};
+        values[field].total = 1;
+        values[field].students = [subValues[0]];
+      }
+    }
+  });
+
+  console.log(`Number of students: ${values.len}`);
+  for (const key in values) {
+    if (values[key].students) {
+      const list = values[key].students.join(', ');
+      console.log(`Number of students in ${key}: ${values[key].total}. List: ${list}`);
+    }
+  }
+}
+
+module.exports = countStudents;
