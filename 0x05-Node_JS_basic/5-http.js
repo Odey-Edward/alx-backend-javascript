f28830275f7a39f6ddd3eebb103590d944769e33
Edward-Odey <edwardodey04@gmail.com>
2024-04-30 14:24:29 +0100

    5-http.js: create countStudent function

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,5 +1,47 @@
 const http = require('http');
-const countStudents = require('./2-read_file');
+const fs = require('fs');
+
+function countStudents(path) {
+  let lines = '';
+
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+
+    lines = (data.split('\n')).slice(1);
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+
+  const values = {};
+
+  values.len = 0;
+
+  lines.forEach((line) => {
+    if (line !== '') {
+      values.len += 1;
+
+      const subValues = line.split(',');
+      const field = subValues.pop();
+
+      if (field in values) {
+        values[field].total += 1;
+        values[field].students.push(subValues[0]);
+      } else {
+        values[field] = {};
+        values[field].total = 1;
+        values[field].students = [subValues[0]];
+      }
+    }
+  });
+
+  console.log(`Number of students: ${values.len}`);
+  for (const key in values) {
+    if (values[key].students) {
+      const list = values[key].students.join(', ');
+      console.log(`Number of students in ${key}: ${values[key].total}. List: ${list}`);
+    }
+  }
+}
 
 const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
