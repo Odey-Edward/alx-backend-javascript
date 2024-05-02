6936a760334c0143dcc7715b7a0997f940ef509e
Edward-Odey <edwardodey04@gmail.com>
2024-05-02 12:20:43 +0100

    1-calcul.test.js: created all test cases

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,35 @@
+const assert = require('assert');
+
+const calculateNumber = require("./1-calcul.js");
+
+describe('calculateNumber function', () => {
+  describe('Two floating numbers', () => {
+    it('arguments 1.4 and 4.5 should return 6', () => {
+      assert.equal(calculateNumber('SUM', 1.4, 4.5), 6);
+    });
+  });
+
+  describe('Subtract two floating number', () => {
+    it('arguments 1.4 and 4.5 should return -4', () => {
+      assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+    });
+  });
+
+  describe('Divide two floating numbers', () => {
+    it('arguments 1.4 and 4.5 should return 0.2', () => {
+      assert.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+    });
+  });
+
+  describe('Divide Two  numbers', () => {
+    it('arguments 1.4 and 0 should return Infinity', () => {
+      assert.equal(calculateNumber('DIVIDE', 1.4, 0), 'Infinity');
+    });
+  });
+
+  describe('Add two negative floating numbers', () => {
+    it('arguments -3.5 and -8.9 should return -12', () => {
+      assert.equal(calculateNumber('SUM', -3.5, -8.9), -12);
+    });
+  });
+});
