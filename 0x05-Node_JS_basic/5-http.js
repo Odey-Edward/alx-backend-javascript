bdacb7dd8ebd82232ba917dd4f86f027e65313f8
Edward-Odey <edwardodey04@gmail.com>
2024-05-03 23:34:03 +0100

    fix task 5

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -17,6 +17,7 @@ const app = http.createServer((req, res) => {
     };
 
     buffer.push('This is the list of our students');
+
     try {
       countStudents(process.argv[2]);
 
@@ -24,7 +25,8 @@ const app = http.createServer((req, res) => {
 
       res.write(buffer.join('\n'));
     } catch (err) {
-      // Do nothing Here
+      buffer.push('Cannot load the database');
+      res.write(buffer.join('\n'));
     }
   }
 
