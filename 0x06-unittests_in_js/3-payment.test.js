42ebad81f41b97addc99fb173db5d14dcfe84e06
Edward-Odey <edwardodey04@gmail.com>
2024-05-03 14:41:32 +0100

    fix test cases for sendPaymentRequestToApi() function

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,16 +1,21 @@
-const { expect } = require('chai');
-const { it, describe } = require('mocha');
 const sinon = require('sinon');
-
+const sendPaymentRequestToApi = require('./3-payment');
+const { describe, it } = require('mocha');
 const Utils = require('./utils.js');
-const sendPaymentRequestToApi = require('./3-payment.js');
 
 describe('sendPaymentRequestToApi', () => {
-  it('checking if numbers round with spies', () => {
-    const checkSoy = sinon.spy(Utils, 'calculateNumber');
-    sendPaymentRequestToApi(1, 3);
-    expect(checkSoy.calledOnce).to.be.true;
-    expect(checkSoy.calledWith('SUM', 1, 3)).to.be.true;
-    checkSoy.restore();
+  const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+
+  sendPaymentRequestToApi(10, 5);
+
+  it('should call calculateNumber', () => {
+    sinon.assert.called(calculateNumberSpy);
   });
+
+  it('should be called with the right arguments', () => {
+    sinon.assert.calledWith(calculateNumberSpy, 'SUM', 10, 5);
+  });
+
+  calculateNumberSpy.restore();
 });
+
